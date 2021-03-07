4658fc2e6e6e4f1dbbbe2d0734dd057aa5503442
ilian <heatblazer@gmail.com>
2021-03-07 12:07:21 +0200

    variardic template parsing

--- Pcap.cpp
+++ Pcap.cpp
@@ -5,8 +5,7 @@
 #include <map>
 #include <string>
 #include <iostream>
-using namespace stun;
-using namespace rtp;
+#include <fstream>
 
 Pcap::Pcap()
 {
@@ -29,7 +28,7 @@ void Pcap::operator ++()
     m_nextRes.data = pcap_next(p_Cap, &m_nextRes.out);
 }
 
-Pcap::Result_t& Pcap::next()
+Result_t& Pcap::next()
 {
     operator++();
     return m_nextRes;
@@ -55,35 +54,21 @@ void Pcap::loop()
     appnames[AppName::Skype] = "Skype";
     appnames[AppName::Unknown] = "Unknown";
 #endif
-    size_t totalStun=0;
-    std::vector<StunRFC> stunCaps;
-    std::vector<RtpRFC> rtpCaps;
-    ;
-    for(Pcap::Result_t& res =  next(); hasNext(); operator++())
+    for(Result_t& res =  next(); hasNext(); operator++())
     {
-        auto resstun = packetHandlerT<StunRFC, StunRFC>(NULL, res, StunRFC{});
-        auto resrtp = packetHandlerT<RtpRFC, RtpRFC>(NULL, res, RtpRFC{});
-        if (resrtp.valid()) {
-            //TODO:
-        }
-       // if (resstun.valid()) {
-        //    totalStun += resstun.count();
+        auto resultNwork = VParse(StunRFC{res},
+                                  RtpRFC{res});
 
-            //AppName n = resstun.app();
-            //if (n != AppName::Unknown)
-            //    std::cout << appnames[n] << "\r\n";
-        //    stunCaps.push_back(resstun);
-        //} else if (resrtp.valid()) {
-
-        //}
-//        memset(&m_nextRes, 0, sizeof(m_nextRes));
     }
-    printf("[%zu total\r\n", totalStun);
+
+    std::ofstream jsonfile;
+    jsonfile.open ("out.json");
+    jsonfile<< serializer.serialize();
+    jsonfile.close();
 }
 
-template<typename T, typename Return_T>
-Return_T Pcap::packetHandlerT(u_char *userData, const Result_t &res, T protocol)
-{
-    (void)userData; //reserved userdata...
-    return protocol(res);
-}
+
+
+
+
+
--- Pcap.h
+++ Pcap.h
@@ -1,17 +1,17 @@
 #ifndef PCAP_H
 #define PCAP_H
 #include "defs.h"
-
+#include "types.h"
+#include "tjson.hpp"
+#include <memory>
 #include <pcap/pcap.h>
+#include <optional>
 
 class Pcap
 {
+
 public:
-    struct Result_t
-    {
-        const u_char* data;
-        struct pcap_pkthdr out;
-    };
+
 
     Pcap();
 
@@ -30,13 +30,49 @@ public:
 protected:
 
     template<typename T, typename Return_T>
-    Return_T packetHandlerT(u_char *userData, const Result_t& res, T protocol);
+    Return_T packetHandlerT(u_char *userData, const Result_t& res, T protocol)
+    {
+        (void)userData; //reserved userdata...
+        return protocol(res);
+    }
+
+    template<typename T>
+    T packetHandlerT(T protocol)
+    {
+        return protocol();
+    }
 
     pcap_t* p_Cap;
 
     char errbuf[PCAP_ERRBUF_SIZE];
 
     Result_t m_nextRes;
+
+private:
+
+    tjson::JsonSerializer serializer;
+
+    Result_t VParse()
+    {
+        return {};
+    }
+
+    template <typename T, class...Args>
+    auto VParse(T value, Args&&...FArgs)// -> decltype (value)
+    {
+        T resultready = packetHandlerT(value);
+        if (resultready.valid()) {
+            value.value.type = resultready.type();
+            serializer.add(resultready.jsonb);
+            return value.value;
+        }
+        // how to not pass the result ready here?
+        else {
+            return VParse(std::forward<Args>(FArgs)...);
+        }
+    }
+
+
 };
 
 #endif // PCAP_H
