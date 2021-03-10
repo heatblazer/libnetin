07e62eed9028eb02f8e113e1e9bcaeaf9f451931
ilian <heatblazer@gmail.com>
2021-03-10 22:18:30 +0200

    test nill rfc parser

---
+++ nill_t.cpp
@@ -0,0 +1,9 @@
+#include "nill_t.h"
+
+
+
+NullRFC &NullRFC::operator()()
+{
+    jsonb.add(tjson::JsonField{"protocol", "NillRFC"});
+    return *this;
+}
---
+++ nill_t.h
@@ -0,0 +1,17 @@
+#ifndef NILL_T_H
+#define NILL_T_H
+#include "types.h"
+#include "tjson.hpp"
+
+struct NullRFC : public IParseable<Result_t>
+{
+    bool valid() const { return false; }
+
+    NullRFC& operator()();
+
+
+    Result_t::TypeRFC type() const { return Result_t::TypeRFC::RTP;}
+
+};
+
+#endif // NILL_T_H
