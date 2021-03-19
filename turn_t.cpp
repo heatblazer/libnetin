675a0cea380d7373b456804e613529fba73d5a36
ilian <heatblazer@gmail.com>
2021-03-19 16:43:45 +0200

    added turn

---
+++ turn_t.cpp
@@ -0,0 +1,60 @@
+#include "turn_t.h"
+#include "stun_t.h"
+#include "rtp_t.h"
+#include "utils.h"
+
+using namespace stun;
+using namespace rtp;
+
+
+namespace turn {
+
+    TurnRFC::TurnRFC()
+    {
+
+    }
+
+    TurnRFC::TurnRFC(const IParseable::type &ref)
+        : IParseable<Result_t>{ref}
+    {
+    }
+
+    TurnRFC &TurnRFC::operator()(const IParseable::type &res)
+    {
+        struct EthL4 eth = utils::GetEthL4(res.data);
+        switch (eth.type) {
+        case EthL4::UDP: {
+                unsigned idx = (sizeof(struct ether_header) + sizeof(struct ip) + sizeof(udphdr));
+                unsigned short chann = (res.data[idx]) << 8 | (res.data[idx+1]);
+                unsigned short len = (res.data[idx+2] << 8) | (res.data[idx+3]);
+                if (chann == 0x8078) {
+                    Valid = true;
+                    jsonb.add(tjson::JsonField{"protocol", "TURN"});
+                    jsonb.add(tjson::JsonField{"srcip", eth.sourceIP});
+                    jsonb.add(tjson::JsonField{"dstip", eth.destIP});
+                    jsonb.add(tjson::JsonField{"chan", chann});
+                    jsonb.add(tjson::JsonField{"length", len});
+
+                    RtpRFC rtp{res} ;
+                    StunRFC stun{res};
+                    if (stun().Valid) {
+                        jsonb.add(tjson::JsonField{"nested-protocol", "STUN in TURN"});
+                    }
+                    if (rtp().Valid) {
+                        jsonb.add(tjson::JsonField{"nested-protocol", "RTP in TURN"});
+                    }
+
+                }
+        }
+        default:
+            break;
+        }
+        return  *this;
+    }
+
+    TurnRFC &TurnRFC::operator()()
+    {
+        return this->operator()(this->value);
+    }
+
+} //turn
---
+++ turn_t.h
@@ -0,0 +1,26 @@
+#ifndef TURN_T_H
+#define TURN_T_H
+
+#include "types.h"
+#include "tjson.hpp"
+
+
+namespace turn {
+
+    struct TurnRFC: public IParseable<Result_t>
+    {
+        TurnRFC();
+
+        TurnRFC(const IParseable::type& ref);
+
+        TurnRFC& operator()(const IParseable::type&);
+
+        TurnRFC& operator()();
+
+        Result_t::TypeRFC type() const { return Result_t::TypeRFC::TURN;}
+
+
+    };
+}
+
+#endif // TURN_T_H
